47f64569b676c9392b29ce2393e9b53a9676f1e3
juliustaha123 <105410360+juliustaha123@users.noreply.github.com>
2023-12-17 21:01:08 +0800

    Update index.php

--- test_sad/index.php
+++ test_sad/index.php
@@ -4,16 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Form</title>
+    <link rel="stylesheet" href="index.css">
+
 </head>
 <body>
+
+     <div class="first-top-container">
+        <h1>JMC Identification Card Request</h1>
+    <div class="second-top-container">
+
     <form action="process_form.php" method="post">
-        <label for="name">Name:</label>
+    <div class="inner-first">
+        <label for="firstname">First name </label>
         <input type="text" name="name" required>
         
-        <label for="email">Email:</label>
-        <input type="email" name="email" required>
+        <label for="MI">MI </label>
+        <input type="MI" name="MI" required>
         
+        <label for="lastname">Last Name </label>
+        <input type="lastname" name="lastname" required>
+    </div>
+
+    <div class="inner-second">
+        <label for="studentaddress">Student Address </label>
+        <input type="text" name="name" required>
+        
+        <label for="cpNumber">Guardian's Contact No.  </label>
+        <input type="cpNumber" name="cpNumber" required>
+        
+        <label for="birthdate">Student's Birth Date</label>
+        <input type="date" id="birthday" name="birthday">
+    </div>
+
+    <div class="submit-container">
         <input type="submit" value="Submit">
+    </div>
+       
     </form>
+    </div>
+    </div>
 </body>
 </html>
