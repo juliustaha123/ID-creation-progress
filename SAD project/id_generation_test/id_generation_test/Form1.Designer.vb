3b956afd6a9455d8d064abf599352d9c60eff0f5
Migi22 <108279314+Migi22@users.noreply.github.com>
2023-12-17 14:49:22 +0800

    Add files via upload
    Beta v1.1
    1.) added ability to select user_ID from the database and then put it to it own textbox.

--- SAD project/id_generation_test/id_generation_test/Form1.Designer.vb
+++ SAD project/id_generation_test/id_generation_test/Form1.Designer.vb
@@ -24,6 +24,12 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.btnRefresh = New System.Windows.Forms.Button()
         Me.dgvUsers = New System.Windows.Forms.DataGridView()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtSelectID = New System.Windows.Forms.TextBox()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblEmail = New System.Windows.Forms.Label()
         CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -39,26 +45,87 @@ Partial Class Form1
         'dgvUsers
         '
         Me.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvUsers.Location = New System.Drawing.Point(76, 87)
+        Me.dgvUsers.Location = New System.Drawing.Point(536, 44)
         Me.dgvUsers.Name = "dgvUsers"
         Me.dgvUsers.RowTemplate.Height = 28
         Me.dgvUsers.Size = New System.Drawing.Size(475, 202)
         Me.dgvUsers.TabIndex = 1
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(141, 53)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(173, 26)
+        Me.txtName.TabIndex = 2
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(74, 59)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(51, 20)
+        Me.lblName.TabIndex = 3
+        Me.lblName.Text = "Name"
+        '
+        'txtSelectID
+        '
+        Me.txtSelectID.Location = New System.Drawing.Point(289, 353)
+        Me.txtSelectID.Name = "txtSelectID"
+        Me.txtSelectID.Size = New System.Drawing.Size(129, 26)
+        Me.txtSelectID.TabIndex = 4
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Location = New System.Drawing.Point(289, 402)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(129, 58)
+        Me.btnSelect.TabIndex = 5
+        Me.btnSelect.Text = "Select"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(141, 100)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(173, 26)
+        Me.txtEmail.TabIndex = 2
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(74, 106)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(46, 20)
+        Me.lblEmail.TabIndex = 3
+        Me.lblEmail.Text = "email"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1179, 540)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.txtSelectID)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.dgvUsers)
         Me.Controls.Add(Me.btnRefresh)
         Me.Name = "Form1"
         Me.Text = "Form1"
         CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents btnRefresh As System.Windows.Forms.Button
     Friend WithEvents dgvUsers As System.Windows.Forms.DataGridView
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtSelectID As System.Windows.Forms.TextBox
+    Friend WithEvents btnSelect As System.Windows.Forms.Button
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents lblEmail As System.Windows.Forms.Label
 
 End Class
--- SAD project/id_generation_test/id_generation_test/Form1.vb
+++ SAD project/id_generation_test/id_generation_test/Form1.vb
@@ -1,7 +1,31 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        ' Load all data into DataGridView
+        LoadDataIntoDataGridView()
+    End Sub
+
+    Private Sub dgvUsers_SelectionChanged(sender As Object, e As EventArgs) Handles dgvUsers.SelectionChanged
+        ' Handle the selection change in the DataGridView
+        If dgvUsers.SelectedRows.Count > 0 Then
+            ' Get the selected user's ID
+            Dim userId As Integer = Convert.ToInt32(dgvUsers.SelectedRows(0).Cells("user_ID").Value)
+
+            ' Load user data based on the selected ID
+            LoadUserDataById(userId)
+        End If
+    End Sub
+
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        ' Load user data based on the entered ID from the TextBox
+        If Not String.IsNullOrEmpty(txtSelectID.Text) Then
+            Dim selectedUserId As Integer = Convert.ToInt32(txtSelectID.Text)
+            LoadUserDataById(selectedUserId)
+        End If
+    End Sub
+
+    Private Sub LoadDataIntoDataGridView()
         Dim connectionString As String = "server=localhost;user=root;password=;database=test_vb_connect;"
         Dim connection As New MySqlConnection(connectionString)
 
@@ -25,5 +49,35 @@ Public Class Form1
         End Try
     End Sub
 
-    
+    Private Sub LoadUserDataById(userId As Integer)
+        Dim connectionString As String = "server=localhost;user=root;password=;database=test_vb_connect;"
+        Dim connection As New MySqlConnection(connectionString)
+
+        Try
+            connection.Open()
+
+            ' Perform database operations to get user data by ID
+            Dim query As String = "SELECT name, email FROM users WHERE user_ID = @userId"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@userId", userId)
+
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+
+            ' Display the result in TextBox
+            If reader.Read() Then
+                txtName.Text = reader("name").ToString()
+                txtEmail.Text = reader("email").ToString()
+            Else
+                txtName.Text = "No data found."
+                txtEmail.Text = "No data found."
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+   
 End Class
