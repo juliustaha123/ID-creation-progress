abe37650121ac8e0efe674ca85c1e7070ca7bd61
Migi22 <108279314+Migi22@users.noreply.github.com>
2023-12-17 16:58:54 +0800

    Add files via upload
    PHP FORM

---
+++ test_sad/process_form.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test_vb_connect";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record added successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
