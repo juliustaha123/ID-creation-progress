11af354cafed2462533022b8808bd324927a7c20
Migi22 <108279314+Migi22@users.noreply.github.com>
2023-12-17 14:25:32 +0800

    Add files via upload
    1.) added web form localhost php
    2.) added mysql connection
    3.) added simple vb.net that can fetch the data from the MySQL database.
    4.) still a test run of function from PHP to database and vb.net

---
+++ SAD project/id_generation_test/id_generation_test/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.dgvUsers = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(76, 402)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(129, 58)
+        Me.btnRefresh.TabIndex = 0
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'dgvUsers
+        '
+        Me.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvUsers.Location = New System.Drawing.Point(76, 87)
+        Me.dgvUsers.Name = "dgvUsers"
+        Me.dgvUsers.RowTemplate.Height = 28
+        Me.dgvUsers.Size = New System.Drawing.Size(475, 202)
+        Me.dgvUsers.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1179, 540)
+        Me.Controls.Add(Me.dgvUsers)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvUsers, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents dgvUsers As System.Windows.Forms.DataGridView
+
+End Class
---
+++ SAD project/id_generation_test/id_generation_test/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        Dim connectionString As String = "server=localhost;user=root;password=;database=test_vb_connect;"
+        Dim connection As New MySqlConnection(connectionString)
+
+        Try
+            connection.Open()
+
+            ' Perform database operations
+            Dim query As String = "SELECT * FROM users"
+            Dim command As New MySqlCommand(query, connection)
+
+            Dim dataTable As New DataTable()
+            dataTable.Load(command.ExecuteReader())
+
+            ' Bind DataTable to DataGridView
+            dgvUsers.DataSource = dataTable
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    
+End Class
